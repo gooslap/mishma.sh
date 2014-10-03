209658ebec68a4a2c523f0bd75ae01d77d105723
mason <mason@gooslap.com>
2014-10-02 20:36:29 -0500

    added mishma.sh

---
+++ mishma.sh
@@ -0,0 +1,121 @@
+# usage: source mishma.sh
+
+_()
+{
+
+    # These are taken from underscore.js which seems like a
+    # reasonable starting point.
+
+    # Objects:
+    # new
+    # keys
+    # values
+    # pairs
+    # invert
+    # functions
+    # extend
+    # pick
+    # omit
+    # defaults
+    # clone
+    # tap
+    # has
+    # matches
+    # property
+    # is_equal
+    # is_empty
+    # is_element
+    # is_array
+    # is_object
+    # is_arguments
+    # is_function
+    # is_string
+    # is_number
+    # is_finite
+    # is_boolean
+    # is_date
+    # is_reg_exp
+    # is_nan
+    # is_undefined
+
+    # Collections:
+    # each
+    # map
+    # reduce
+    # filter_right
+    # find
+    # filter
+    # where
+    # findWhere
+    # reject
+    # every
+    # some
+    # contains
+    # invoke
+    # pluck
+    # max
+    # min
+    # sort_by
+    # group_by
+    # index_by
+    # count_by
+    # shuffle
+    # sample
+    # to_array
+
+    # Arrays:
+    # first
+    # initial
+    # last
+    # rest
+    # compact
+    # flatten
+    # without
+    # union
+    # intersection
+    # difference
+    # uniq
+    # zip
+    # object
+    # index_of
+    # last_index_of
+    # sorted_index
+    # range
+
+    # Functions:
+    # bind
+    # bind_all
+    # partial
+    # memoize
+    # delay
+    # defer
+    # throttle
+    # debounce
+    # once
+    # after
+    # before
+    # wrap
+    # negate
+    # compose
+
+    # Utility:
+    # no_conflict
+    # identity
+    # constant
+    # noop
+    # times
+    # random
+    # mixin
+    # iteratee
+    # unique_id
+    # escape
+    # unescape
+    # result
+    # now
+    # template
+
+    # Chaining:
+    # chain
+    # value
+
+}
